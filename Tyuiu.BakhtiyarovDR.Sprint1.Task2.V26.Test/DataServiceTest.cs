a11ec89c7e812e46019d1df82b31f903c0ba42c2
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-09-21 22:12:52 +0500

    Task2

---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26.Lib;
+
+//ISprint1Task2V26.CalculateMinutesSinceStart(int, int)
+
+namespace Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int value = 21;
+            int valueTwo = 50;
+            var res = ds.CalculateMinutesSinceStart(value, valueTwo);
+            Assert.AreEqual(1310, res);
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26.Lib;
+
+
+// ЗАДАНИЕ
+// Написать программу, которая запрашивает у пользователя исходныен данные,
+// выполняет указанные расчеты и печатает результат на экране.
+
+
+namespace Tyuiu.BakhtiyarovDR.Sprint1.Task2.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Бахтияров Д. Р. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value;
+
+            Console.WriteLine("Введите значение (час) ");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            int valueTwo;
+
+            Console.WriteLine("Введите значение (минуты) ");
+            valueTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                                ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество минут прошло с начала суток = " + ((value * 60) + valueTwo));
+
+            Console.ReadLine();
+        }
+    }
+}
