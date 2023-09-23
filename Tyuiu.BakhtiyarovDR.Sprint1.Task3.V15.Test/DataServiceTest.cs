77a008fd1a849a0aa4ae569b6163aa1a736ed91b
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-09-23 22:27:38 +0500

    Task3

---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Lib;
+
+// s=s0-t*(v1+v2)
+// s=5-2*(50+80)=390
+
+namespace Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double v1 = 2;
+            double v2 = 2;
+            double s = 2;
+            double t = 2;
+            double wait = 10;
+            var res = ds.DistanceOverTime(v1, v2, s, t);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BakhtiyarovDR.Sprint1.Task3.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a01c8416-ca6b-40f0-828a-be3cb36c7453")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
