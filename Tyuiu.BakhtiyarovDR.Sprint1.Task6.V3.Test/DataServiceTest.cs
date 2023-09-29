cda6f5b84aff33b8cdb5c2e765117058c478bd8f
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-09-29 19:30:05 +0500

    Task6

---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Lib;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "бим бим бам бам";
+            DataService ds = new DataService();
+            string res = ds.LastLetterWord(strTest);
+            string wait = "мммм";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BakhtiyarovDR.Sprint1.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("711ea0af-2ccd-4455-87c9-f7dd43cacce3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
